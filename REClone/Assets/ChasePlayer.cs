c622a08afa94ed7fea3d3f6919caeb1bb5dade5a
Zach-Kolman <zach.kolman@gmail.com>
2021-10-07 09:04:46 -0500

    enemy states

--- REClone/Assets/ChasePlayer.cs
+++ REClone/Assets/ChasePlayer.cs
@@ -7,6 +7,8 @@ public class ChasePlayer : MonoBehaviour
 {
     private GameObject player;
     NavMeshAgent agent;
+    public GameObject randoChecker;
+    public GameObject tawkeeky;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +21,25 @@ public class ChasePlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        WanderAround();
+    }
+
+    public void ChaseThePlayer()
+    {
         agent.destination = player.transform.position;
+    }
+
+    public void WanderAround()
+    {
+        tawkeeky.transform.position = RandomPointInBounds(randoChecker.GetComponent<Collider>().bounds);
+    }
+
+    private Vector3 RandomPointInBounds(Bounds bounds)
+    {
+        return new Vector3(
+        Random.Range(bounds.min.x, bounds.max.x),
+        Random.Range(bounds.min.y, bounds.max.y),
+        Random.Range(bounds.min.y, bounds.max.y)
+        );
     }
 }
--- REClone/Assets/EnemyBase.cs
+++ REClone/Assets/EnemyBase.cs
@@ -7,41 +7,52 @@ public class EnemyBase : MonoBehaviour
 {
     public float maxHealth;
     public float curHealth;
-
     public bool gotShot = false;
-
     GameObject player;
-
-    public float poseSpeed;
-
+    private float poseSpeed;
     private Animator anim;
-
-    private NavMeshAgent agent;
-
-    private Vector3 prevAgentPos;
-
-    public float parentSpeed;
-    // Start is called before the first frame update
-    void Awake()
+    //private NavMeshAgent agent;
+    //private Vector3 prevAgentPos;
+    private float parentSpeed = 0;
+    public float distanceFromPlayer;
+    private float playerDist;
+    public enum distState
     {
-        curHealth = maxHealth;
+        Wandering,
+        Chasing,
+        Attacking,
+    }
+    public distState curState;
+
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("hi");
         player = GameObject.FindGameObjectWithTag("Player").gameObject;
         poseSpeed = 0;
         anim = GetComponent<Animator>();
+       //agent = transform.parent.GetComponent<NavMeshAgent>();
+        curHealth = maxHealth;
+        //GetDistanceFromPlayer();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //print("PlayerDist is: " + playerDist);
+        distanceFromPlayer = playerDist;
+        SwitchStates();
+        //SetStates();
         takeDamage();
-        die();
-
+        StartCoroutine("die");
+        GetDistanceFromPlayer();
     }
 
     private void FixedUpdate()
     {
         parentSpeed = transform.parent.GetComponent<CheckNMAVel>().velly;
-        anim.SetFloat("PoseSpeed", poseSpeed);
+        anim.SetFloat("PoseSpeed", value: poseSpeed);
     }
 
     public void takeDamage()
@@ -69,12 +80,60 @@ public class EnemyBase : MonoBehaviour
        
     }
 
-    void die()
+    IEnumerator die()
     {
+        yield return new WaitForSeconds(1);
         if(curHealth <= 0)
         {
             transform.parent.gameObject.SetActive(false);
         }
        
     }
+
+    void GetDistanceFromPlayer()
+    {
+        playerDist = Vector3.Distance(transform.position, player.transform.position);
+    }
+
+    void SwitchStates()
+    {
+        if(playerDist <= 2.5)
+        {
+            print("take that scum");
+        }
+
+        if(playerDist <= 10)
+        {
+            print("run bish");
+        }
+
+        if(playerDist > 14)
+        {
+            gameObject.transform.parent.GetComponent<ChasePlayer>().WanderAround();
+            print("poopy wandering");
+        }
+    }
+
+    void ChasePlayer()
+    {
+        gameObject.GetComponent<ChasePlayer>().ChaseThePlayer();
+    }
+
+    //void SetStates()
+    //{
+    //    if(playerDist <= 2)
+    //    {
+    //        curState = distState.Attacking;
+    //    }
+
+    //    if (playerDist <= 7)
+    //    {
+    //        curState = distState.Chasing;
+    //    }
+
+    //    if (playerDist > 7)
+    //    {
+    //        curState = distState.Wandering;
+    //    }
+    //}
 }
